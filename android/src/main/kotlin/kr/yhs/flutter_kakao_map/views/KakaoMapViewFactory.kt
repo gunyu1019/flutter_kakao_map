ddb208edd2b969f4ad0887a1f180e7e6165b9c37
gunyu1019 <gunyu1019@gmail.com>
2024-11-29 03:08:22 +0900

    [Feat] KakaoMapViewFactory

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/views/KakaoMapViewFactory.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/views/KakaoMapViewFactory.kt
@@ -1,14 +1,27 @@
 package kr.yhs.flutter_kakao_map.views
 
+import android.app.Activity
 import android.content.Context
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class KakaoMapViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class KakaoMapViewFactory(
+    private val activity: Activity,
+    private val messenger: BinaryMessenger
+) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 
     override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        val channel = MethodChannel(messenger, "flutter_kakao_map_view#${viewId}")
         val creationParams = args as Map<String, Any?>?
-        return KakaoMapView(context, viewId, creationParams)
+        return KakaoMapView(
+            activity = activity,
+            context = context,
+            viewId = viewId,
+            creationParams = creationParams,
+            channel = channel
+        )
     }
 }
