45f26dba985a27de90a9fa50237b7c066219942b
gunyu1019 <gunyu1019@gmail.com>
2024-11-30 15:22:31 +0000

    [Feat] Add onMethodCall handler

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/sdk/SdkInitializer.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/sdk/SdkInitializer.kt
@@ -3,13 +3,32 @@ package kr.yhs.flutter_kakao_map.SDK
 import android.content.Context
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodCall
+import kotlin.Result
+import com.kakao.vectormap.KakaoMapSdk
 
 class SdkInitializer(
     private val context: Context,
     private val messenger: BinaryMessenger
-) {
+): MethodChannel.MethodCallHandler {
+    private val channel: MethodChannel = MethodChannel(messenger, SDK_CHANNEL_NAME)
+    
     init {
-        val channel = MethodChannel(context, SDK_CHANNEL_NAME)
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(method: MethodCall, result: MethodChannel.Result) {
+        if (method.method == "initalize") {
+            val appKey: String? = method.argument("appKey")
+
+            if (appKey == null) {
+                result.error("TypeError", "Unknown argument: appKey", null);
+                return
+            }
+
+            KakaoMapSdk.init(context, appKey)
+            result.success(null)
+        }
     }
 
     companion object {
