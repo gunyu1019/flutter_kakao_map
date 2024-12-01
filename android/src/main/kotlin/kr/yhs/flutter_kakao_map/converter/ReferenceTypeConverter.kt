d1616a448d7ca17b49df02b1825bfedf628fae60
gunyu1019 <gunyu1019@gmail.com>
2024-12-01 11:28:16 +0000

    [Feat] Add CameraPosition ReferenceTypeConverter

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ReferenceTypeConverter.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ReferenceTypeConverter.kt
@@ -1,6 +1,8 @@
 package kr.yhs.flutter_kakao_map.converter
 
 import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.camera.CameraPosition
+import kotlin.Double
 
 
 object ReferenceTypeConverter {
@@ -14,5 +16,25 @@ object ReferenceTypeConverter {
     fun LatLng.toMessageable(): Map<String, Double> = mapOf(
         "latitude" to latitude,
         "longitude" to longitude
+    )
+
+    fun Any.asCameraPosition(): CameraPosition = asMap().let { map: Map<String, Any> ->
+        CameraPosition.from(
+            map["latitude"] as Double,
+            map["longitude"] as Double,
+            map["zoomLevel"] as Int,
+            map["tiltAngle"] as Double,
+            map["rotationAngle"] as Double,
+            map["height"] as Double,
+        )
+    }
+
+    fun CameraPosition.toMessageable(): Map<String, Any> = mapOf(
+        "latitude" to position.latitude,
+        "longitude" to position.longitude,
+        "zoomLevel" to zoomLevel
+        "tiltAngle" to tiltAngle,
+        "rotationAngle" to rotationAngle,
+        "height" to height
     )
 }
