d3da8c15a8fffa61d3fadf7c5ca99d8e166685c2
gunyu1019 <gunyu1019@gmail.com>
2024-12-01 04:42:57 +0000

    [Feat] (WIP) #2 - Camera Controller Handler

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/controller/KakaoMapControllerHandler.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/controller/KakaoMapControllerHandler.kt
@@ -1,1 +1,9 @@
+package kr.yhs.flutter_kakao_map.controller
 
+import com.kakao.vectormap.MapLifeCycleCallback
+
+internal interface KakaoMapControlHandler {
+    fun getCameraPosition()
+
+    fun moveCamera()
+}
