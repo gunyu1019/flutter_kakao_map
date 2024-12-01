80412457fad91a601a2cb515d0f2abaab772cda3
gunyu1019 <gunyu1019@gmail.com>
2024-12-01 11:17:16 +0000

    [Feat] Add ReferenceTypeConverter

---
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ReferenceTypeConverter.kt
@@ -0,0 +1,18 @@
+package kr.yhs.flutter_kakao_map.converter
+
+import com.kakao.vectormap.LatLng
+
+
+object ReferenceTypeConverter {
+    fun Any.asLatLng(): LatLng = asMap<Double>().let { map ->
+        LatLng.from(
+            map["latitude"],
+            map["longitude"]
+        )
+    }
+
+    fun LatLng.toMessageable(): Map<String, Double> = mapOf(
+        "latitude" to latitude,
+        "longitude" to longitude
+    )
+}
