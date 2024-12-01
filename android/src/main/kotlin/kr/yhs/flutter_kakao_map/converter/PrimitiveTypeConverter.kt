b4064a28799bc3fa5a974e70ddf79013d407f322
gunyu1019 <gunyu1019@gmail.com>
2024-12-01 11:17:10 +0000

    [Feat] Add PrimitiveTypeConverter

---
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/PrimitiveTypeConverter.kt
@@ -0,0 +1,11 @@
+package kr.yhs.flutter_kakao_map.converter
+
+object PrimitiveTypeConverter {
+    fun Any.asBoolean(): Boolean = this as Boolean
+    fun Any.asDouble(): Double = this as Double
+    fun Any.asInt(): Int = if (this is Long) toInt() else this as Int
+    fun Any.asLong(): Long = if (this is Int) toLong() else this as Long
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> Any.asMap(): Map<String, T> = this as Map<String, T>
+}
