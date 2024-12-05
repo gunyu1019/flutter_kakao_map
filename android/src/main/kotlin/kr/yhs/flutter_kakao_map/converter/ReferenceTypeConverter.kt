bcc00dc6a4ab7212918dc69ac62edd0eb23616a1
gunyu1019 <gunyu1019@gmail.com>
2024-12-05 11:20:03 +0000

    [Feat] #2 - Add asCameraAnimation() method at ReferenceTypeConverter

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ReferenceTypeConverter.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ReferenceTypeConverter.kt
@@ -2,6 +2,7 @@ package kr.yhs.flutter_kakao_map.converter
 
 import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.camera.CameraPosition
+import com.kakao.vectormap.camera.CameraAnimation
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asMap
 
 
@@ -37,4 +38,12 @@ object ReferenceTypeConverter {
         "rotationAngle" to rotationAngle,
         "height" to height
     )
+
+    fun Any.asCameraAnimation(): CameraAnimation = asMap().let { map: Map<String, Any?> ->
+        CameraAnimation.from(
+            map["duration"] as Int,
+            map["autoElevation"] as Boolean? ?: false, 
+            map["isConsecutive"] as Boolean? ?: false
+        )
+    }
 }
