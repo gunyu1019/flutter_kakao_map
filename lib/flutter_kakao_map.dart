ded4dbf40c8da885bc0ea82bb3e19ae41752669e
gunyu1019 <gunyu1019@gmail.com>
2024-12-12 10:29:16 +0000

    [Feat] Add two types at flutter_kakao_map.dart

--- lib/flutter_kakao_map.dart
+++ lib/flutter_kakao_map.dart
@@ -6,4 +6,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
+part 'type/enums.dart';
+part 'type/map_option.dart';
+
 part 'widget/map_widget.dart';
