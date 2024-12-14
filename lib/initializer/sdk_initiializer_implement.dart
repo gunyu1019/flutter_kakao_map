cbf7db9d849eda71761bdacaca0d09129ed8c03d
gunyu1019 <gunyu1019@gmail.com>
2024-12-14 05:58:55 +0000

    [Feat] Implement isInitialize method and hashKey method (dart)

--- lib/initializer/sdk_initiializer_implement.dart
+++ lib/initializer/sdk_initiializer_implement.dart
@@ -1,13 +1,25 @@
 part of '../flutter_kakao_map.dart';
 
 class KakaoMapSdkImplement implements KakaoMapSdk {
+  MethodChannel channel = ChannelType.sdk.channel;
+
   @override
-  Future<String> hashKey() {
-    return "";
+  Future<String?> hashKey() async {
+    if (!Platform.isAndroid) {
+      return null;
+    }
+    return await channel.invokeMethod("hashKey");
   }
 
   @override
-  Future<void> initialize(String token) {
-    
+  Future<void> initialize(String appKey) async {
+    await channel.invokeMethod("initialize", {
+      "appKey": appKey
+    });
+  }
+  
+  @override
+  Future<bool> isInitialize() async {
+    return await channel.invokeMethod("isInitialize");
   }
 }
