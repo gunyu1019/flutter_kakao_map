1e7b2b72ec7e23fe82895916d2eeee68caacb0e4
gunyu1019 <gunyu1019@gmail.com>
2024-12-14 09:11:35 +0000

    [Feat] (Example) Add TextStyle

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_kakao_map/flutter_kakao_map.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await dotenv.load(fileName: 'assets/config/.env');
   await KakaoMapSdk.instance.initialize(dotenv.env['KAKAO_API_KEY']!);
 
@@ -22,22 +22,29 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 10.0);
+    const textStyle = TextStyle(
+        fontSize: 14.0, color: Colors.blue, decoration: TextDecoration.none);
+    var mediaQueryData = MediaQuery.of(context);
+    var screenWidth = mediaQueryData.size.width;
+    var screenHeight = mediaQueryData.size.height;
 
     return MaterialApp(
       home: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
+        width: screenWidth,
+        height: screenHeight,
         child: Column(
           children: [
-            FutureBuilder(future: KakaoMapSdk.instance.hashKey(), builder: (BuildContext context, AsyncSnapshot snapshot) {
-              return (
-                snapshot.hasData ?
-                Text(snapshot.data ?? "로딩 실패", style: textStyle) :
-                const Text("로딩 중", style: textStyle)
-              );
-            }),
-            const KakaoMap()
+            FutureBuilder(
+                future: KakaoMapSdk.instance.hashKey(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return (snapshot.hasData
+                      ? Text(snapshot.data ?? "로딩 실패", style: textStyle)
+                      : const Text("로딩 중", style: textStyle));
+                }),
+            SizedBox(
+                width: screenWidth,
+                height: screenHeight * 0.8,
+                child: const KakaoMap())
           ],
         ),
       ),
