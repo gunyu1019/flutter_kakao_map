899182e56d93a429f9166daf9dd4f842e1a8f8cd
gunyu1019 <gunyu1019@gmail.com>
2024-12-14 05:43:59 +0000

    [Feat] Add SdkInitializerHandler

---
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/sdk/SdkInitializerHandler.kt
@@ -0,0 +1,19 @@
+package kr.yhs.flutter_kakao_map.sdk
+
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodCall
+
+interface SdkInitializerHandler {
+    fun handle(call: MethodCall, result: MethodChannel.Result) = when (call.method) {
+        "initialize" -> initialize(call.argument("appKey")!!, result)
+        "isInitialize" -> isInitialize(result)
+        "hashKey" -> hashKey(result)
+        else -> result.notImplemented()
+    }
+
+    fun initialize(appKey: String, result: MethodChannel.Result);
+
+    fun isInitialize(result: MethodChannel.Result);
+
+    fun hashKey(result: MethodChannel.Result);
+}
