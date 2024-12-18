f2621236d74ccdb5aac8ef3d8db62f5ffd63f650
gunyu1019 <gunyu1019@gmail.com>
2024-12-18 10:58:11 +0000

    [Feat] #2 - Add Camera Animation class

---
+++ lib/models/camera/camera_animation.dart
@@ -0,0 +1,24 @@
+part of '../../flutter_kakao_map.dart';
+
+
+class CameraAnimation {
+  final int duration;
+  final bool autoElevation;
+  final bool isConsecutive;
+
+  const CameraAnimation(this.duration, {
+    this.autoElevation = false,
+    this.isConsecutive = false
+  });
+
+  Map<String, dynamic> toMessageable() {
+    return {
+      "duration": duration,
+      "autoElevation": autoElevation,
+      "isConsecutive": isConsecutive,
+    }
+  }
+
+  @override
+  String toString() => "$runtimeType: ${toMessageable().map}";
+}
