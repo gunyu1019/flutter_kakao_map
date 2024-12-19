748aaa94fc49a5032cc4eedbfd6c38af4064e8ce
gunyu1019 <gunyu1019@gmail.com>
2024-12-19 11:09:08 +0000

    [Feat] Used initSurfaceAndroidView

--- lib/widget/platform_view.dart
+++ lib/widget/platform_view.dart
@@ -1,42 +1,40 @@
 part of '../flutter_kakao_map.dart';
 
-
 StatefulWidget _createPlatformView({
   required String viewType,
   Function(int)? onPlatformViewCreated,
-  Map<String, dynamic> creationParams = const {}, 
+  Map<String, dynamic> creationParams = const {},
   MessageCodec creationParamsCodec = const StandardMessageCodec(),
 }) {
   if (Platform.isAndroid) {
-      return PlatformViewLink(
-          surfaceFactory: (context, controller) {
-            return AndroidViewSurface(
-                controller: controller as AndroidViewController,
-                hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-                gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{});
-          },
-          onCreatePlatformView: (params) {
-            return PlatformViewsService.initAndroidView(
-              id: params.id,
-              viewType: viewType,
-              layoutDirection: TextDirection.ltr,
-              creationParams: creationParams,
-              creationParamsCodec: creationParamsCodec,
-              onFocus: () {
-                params.onFocusChanged(true);
-              },
-            )
-              ..addOnPlatformViewCreatedListener((viewId) {
-                params.onPlatformViewCreated(viewId);
-                onPlatformViewCreated?.call(viewId);
-              })
-              ..create();
-          },
-          viewType: viewType);
-    } else if (Platform.isIOS) {
-      // TODO
-      throw UnimplementedError("TODO");
-    } else {
-      throw PlatformException(code: "unsupportedPlatform");
-    }
+    return PlatformViewLink(
+        surfaceFactory: (context, controller) => AndroidViewSurface(
+            controller: controller as AndroidViewController,
+            hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+            gestureRecognizers: const <Factory<
+                OneSequenceGestureRecognizer>>{}),
+        onCreatePlatformView: (params) {
+          return PlatformViewsService.initSurfaceAndroidView(
+            id: params.id,
+            viewType: viewType,
+            layoutDirection: TextDirection.ltr,
+            creationParams: creationParams,
+            creationParamsCodec: creationParamsCodec,
+            onFocus: () {
+              params.onFocusChanged(true);
+            },
+          )
+            ..addOnPlatformViewCreatedListener((viewId) {
+              params.onPlatformViewCreated(viewId);
+              onPlatformViewCreated?.call(viewId);
+            })
+            ..create();
+        },
+        viewType: viewType);
+  } else if (Platform.isIOS) {
+    // TODO
+    throw UnimplementedError("TODO");
+  } else {
+    throw PlatformException(code: "unsupportedPlatform");
+  }
 }
