15d62c9ead3a4ce5f4023ddc6f41e5258f06daa2
gunyu1019 <gunyu1019@gmail.com>
2024-12-21 11:29:55 +0000

    [Feat] #3 - Add CameraListener.kt

---
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/listeners/CameraListener.kt
@@ -0,0 +1,31 @@
+package kr.yhs.flutter_kakao_map.listener
+
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.GestureType
+import com.kakao.vectormap.camera.CameraPosition
+import io.flutter.plugin.common.MethodChannel
+import kr.yhs.flutter_kakao_map.converter.ReferenceTypeConverter.toMessageable
+import kr.yhs.flutter_kakao_map.model.KakaoMapEvent
+
+
+class CameraListener(
+    private val channel: MethodChannel
+) : KakaoMap.OnCameraMoveStartListener, KakaoMap.OnCameraMoveEndListener {
+    override fun onCameraMoveStart(kakaoMap: KakaoMap, gestureType: GestureType) {
+        channel.invokeMethod("onCameraMoveStart", mapOf(
+            "gesture" to gestureType.value
+        ))
+    }
+    
+    override fun onCameraMoveEnd(kakaoMap: KakaoMap, cameraPosition: CameraPosition, gestureType: GestureType) {
+        channel.invokeMethod("onCameraMoveStart", mapOf(
+            "gesture" to gestureType.value,
+            "position" to cameraPosition.toMessageable()
+        ))
+    }
+
+    companion object {
+        const ON_CAMERA_MOVE_START_EVENT = KakaoMapEvent.CameraMoveStart
+        const ON_CAMERA_MOVE_END_EVENT = KakaoMapEvent.CameraMoveEnd
+    }
+}
