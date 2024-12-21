71fc9f14b2bfc59c8aa963536d9c42e19dd0dece
gunyu1019 <gunyu1019@gmail.com>
2024-12-21 15:07:47 +0000

    [Feat] #3 - Add EventController at dart.

---
+++ lib/controller/event/event_controller.dart
@@ -0,0 +1,34 @@
+part of '../../flutter_kakao_map.dart';
+
+
+class KakaoMapEventController with KakaoMapEventControllerHandler, KakaoMapEventControllerSender {
+  final MethodChannel channel;
+
+  void Function(GestureType gestureType)? onCameraMoveStartHandler;
+  void Function(CameraPosition position, GestureType gestureType)? onCameraMoveEndHandler;
+
+  KakaoMapEventController(this.channel) {
+    channel.setMethodCallHandler(handle);
+  }
+
+  @override
+  Future<void> setEventHandler(
+    EventType type,
+    bool enable
+  ) async {
+    await channel.invokeMethod("setEventHandler", {
+      "eventType": type.id,
+      "enable": enable
+    });
+  }
+  
+  @override
+  void onCameraMoveStart(GestureType gestureType) {
+    onCameraMoveStartHandler?.call(gestureType);
+  }
+
+  @override
+  void onCameraMoveEnd(CameraPosition position, GestureType gestureType) {
+    onCameraMoveEndHandler?.call(position, gestureType);
+  }
+}
