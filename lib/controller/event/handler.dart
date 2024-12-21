6e5cf5787d67793795a87e43bcd2cfd7aab1a8fa
gunyu1019 <gunyu1019@gmail.com>
2024-12-21 23:49:46 +0000

    [Feat] #3 - Setup event controller handler

--- lib/controller/event/handler.dart
+++ lib/controller/event/handler.dart
@@ -5,7 +5,22 @@ mixin KakaoMapEventControllerHandler {
   Future<dynamic> handle(MethodCall method) async {
     switch(method.method) {
       case "onCameraMoveStart":
+        final arguments = method.arguments;
+        final gesture = GestureType.values.firstWhere(
+          (el) => arguments['gesture'] as int == el.value,
+          orElse: () => GestureType.unknown
+        );
+        onCameraMoveStart(gesture);
+        break;
       case "onCameraMoveEnd":
+        final arguments = method.arguments;
+        final position = CameraPosition.fromMessageable(arguments['position']);
+        final gesture = GestureType.values.firstWhere(
+          (el) => arguments['gesture'] as int == el.value,
+          orElse: () => GestureType.unknown
+        );
+        onCameraMoveEnd(position, gesture);
+        break;
       default:
         break;
     }
