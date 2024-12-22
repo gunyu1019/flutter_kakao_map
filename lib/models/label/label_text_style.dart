7c84739668a8010c0640a5525fbbf917c83fc385
gunyu1019 <gunyu1019@gmail.com>
2024-12-22 10:50:11 +0000

    [Feat] #4 - Add Label Text Style

---
+++ lib/models/label/label_text_style.dart
@@ -0,0 +1,23 @@
+part of '../../flutter_kakao_map.dart';
+
+class LabelTextStyle {
+  final double aspectRatio;
+  final double characterSpace;
+  final int color;
+  final String font;
+  final double lineSpace;
+  final double size;
+  final int stroke;
+  final int strokeColor;
+
+  LabelTextStyle({
+    this.aspectRatio = 0,
+    this.characterSpace = 0,
+    this.color = -16777216,
+    this.font = "",
+    this.lineSpace = 1.0,
+    this.size = 24,
+    this.stroke = 0,
+    this.strokeColor = 0
+  });
+}
