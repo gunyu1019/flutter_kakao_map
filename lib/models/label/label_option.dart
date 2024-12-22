3e0a6a2af03d06241b44c31f35fbe75566ff8cb9
gunyu1019 <gunyu1019@gmail.com>
2024-12-22 10:50:27 +0000

    [Feat] #4 - Add Label Option (WIP)

---
+++ lib/models/label/label_option.dart
@@ -0,0 +1,10 @@
+part of '../../flutter_kakao_map.dart';
+
+class LabelOption {
+  final LatLng position;
+  final bool clickable;
+  final T
+  final String text;
+  final String tag;
+  final bool visable;
+}
