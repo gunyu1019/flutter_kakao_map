17f65ba8fcacf87555da785cd837c28eecf46b62
gunyu1019 <gunyu1019@gmail.com>
2024-12-24 14:46:23 +0000

    [Feat] Add getAssets static method at FlutterKakaoMapPlugin.

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/FlutterKakaoMapPlugin.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/FlutterKakaoMapPlugin.kt
@@ -7,6 +7,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterAssets
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -14,16 +15,17 @@ import io.flutter.plugin.common.MethodChannel.Result
 import kr.yhs.flutter_kakao_map.views.KakaoMapViewFactory
 import kr.yhs.flutter_kakao_map.SDK.SdkInitializer
 import java.lang.Exception
+import java.io.InputStream
 
 /** FlutterKakaoMapPlugin */
 class FlutterKakaoMapPlugin: FlutterPlugin, ActivityAware {
-    private lateinit var pluginBinding: FlutterPluginBinding
     private lateinit var sdkInitalizer: SdkInitializer
     
     private val context get() = pluginBinding.applicationContext
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         pluginBinding = binding
+        flutterAssets = binding.flutterAssets
 
         // Initalize SDK
         val channel = MethodChannel(binding.binaryMessenger, SDK_CHANNEL_NAME)
@@ -51,9 +53,15 @@ class FlutterKakaoMapPlugin: FlutterPlugin, ActivityAware {
         private const val BASE_CHANNEL_NAME = "flutter_kakao_map"
         private const val SDK_CHANNEL_NAME = "${BASE_CHANNEL_NAME}_sdk"
         private const val VIEW_CHANNEL_NAME = "${BASE_CHANNEL_NAME}_view"
-        private const val EVENT_CHANNEL_NAME = "${BASE_CHANNEL_NAME}_event"
 
         internal fun createViewChannelName(viewId: Int) = "${VIEW_CHANNEL_NAME}#${viewId}"
-        internal fun createEventChannelName(viewId: Int) = "${EVENT_CHANNEL_NAME}#${viewId}"
+
+        private lateinit var flutterAssets: FlutterAssets
+        private lateinit var pluginBinding: FlutterPluginBinding
+
+        internal fun getAsset(named: String): InputStream {
+            val path = flutterAssets.getAssetFilePathByName(named)
+            return pluginBinding.applicationContext.assets.open(path)
+        }
     }
 }
