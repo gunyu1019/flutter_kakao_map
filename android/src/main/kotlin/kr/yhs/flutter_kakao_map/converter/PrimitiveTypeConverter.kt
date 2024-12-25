4c3dd8315888971be103f3532e77e42d63478d05
gunyu1019 <gunyu1019@gmail.com>
2024-12-25 07:43:26 +0000

    [Fix] cause asFloat casting

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/PrimitiveTypeConverter.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/PrimitiveTypeConverter.kt
@@ -2,8 +2,8 @@ package kr.yhs.flutter_kakao_map.converter
 
 object PrimitiveTypeConverter {
     fun Any.asBoolean(): Boolean = this as Boolean
-    fun Any.asDouble(): Double = this as Double
-    fun Any.asFloat(): Float = this as Float
+    fun Any.asDouble(): Double = if (this is Float) toDouble() else this as Double
+    fun Any.asFloat(): Float = if (this is Double) toFloat() else this as Float
     fun Any.asString(): String = this as String
     fun Any.asInt(): Int = if (this is Long) toInt() else this as Int
     fun Any.asLong(): Long = if (this is Int) toLong() else this as Long
