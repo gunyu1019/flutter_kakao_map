5ba0435652182f858132898fa05c4b827a4d9857
gunyu1019 <gunyu1019@gmail.com>
2024-12-25 09:48:27 +0000

    [Feat] #4 - Add height and width argument at KImage.

--- lib/models/base/image.dart
+++ lib/models/base/image.dart
@@ -5,20 +5,27 @@ class KImage {
   final Uint8List? data;
   final ImageType type;
 
+  final double width;
+  final double height;
+
   const KImage._(this.type, {
+    required this.width,
+    required this.height,
     this.path,
-    this.data
+    this.data,
   });
 
-  factory KImage.fromAsset(String asset) => KImage._(ImageType.assets, path: asset);
+  factory KImage.fromAsset(String asset, double width, double height) => KImage._(ImageType.assets, path: asset, width: width, height: height);
   
-  factory KImage.fromFile(Uint8List data) => KImage._(ImageType.data, data: data);
+  factory KImage.fromFile(Uint8List data, double width, double height) => KImage._(ImageType.data, data: data, width: width, height: height);
 
-  factory KImage.fromPath(String path) => KImage._(ImageType.file, path: path);
+  factory KImage.fromPath(String path, double width, double height) => KImage._(ImageType.file, path: path, width: width, height: height);
   
   Map<String, dynamic> toMessageable() {
     final payload = <String, dynamic>{
       "type": type.value,
+      "width": width,
+      "height": height
     };
 
     if (type == ImageType.data) {
