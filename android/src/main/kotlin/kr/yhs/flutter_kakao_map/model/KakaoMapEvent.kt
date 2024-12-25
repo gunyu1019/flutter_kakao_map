db90891909e1c9356e4a286bf71506997e3ad644
gunyu1019 <gunyu1019@gmail.com>
2024-12-25 04:18:09 +0000

    [Fix] Missing semi-colunm at enumeration class KakaoMapEvent.

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/model/KakaoMapEvent.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/model/KakaoMapEvent.kt
@@ -2,7 +2,7 @@ package kr.yhs.flutter_kakao_map.model
 
 enum class KakaoMapEvent(val id: Int) {
     CameraMoveStart(1),
-    CameraMoveEnd(2)
+    CameraMoveEnd(2);
     
     fun compare(other: Int): Boolean { 
         return (this.id and other) == this.id;
