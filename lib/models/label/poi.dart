ccf5f7f5346a1336923997bb8ebd8049ed8180fc
gunyu1019 <gunyu1019@gmail.com>
2024-12-27 11:28:32 +0000

    [Feat] (WIP) Work on Process Poi  (TODO: add control feature.)

--- lib/models/label/poi.dart
+++ lib/models/label/poi.dart
@@ -2,5 +2,14 @@ part of '../../flutter_kakao_map.dart';
 
 class Poi {
   final LabelController _controller;
-  final PoiOption _option;
+  final String? id;
+  
+  final TransformMethod? transform;
+  LatLng position;
+  bool clickable;
+  String? text;
+  int rank;
+  String? styleId;
+  List<PoiStyle>? styles;
+  bool visible;
 }
