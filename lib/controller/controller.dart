d1e4d46021a34a696cba4f21966f50e6018b9409
gunyu1019 <gunyu1019@gmail.com>
2025-01-11 13:52:05 +0000

    [Feat] Implement OverlayManager feature at KakaoMapcontroller.

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,7 +1,9 @@
 part of '../flutter_kakao_map.dart';
 
-class KakaoMapController with KakaoMapControllerSender {
+class KakaoMapController with KakaoMapControllerSender, OverlayManager {
   final MethodChannel channel;
+  
+  @override
   final MethodChannel overlayChannel;
 
   KakaoMapController(this.channel, {required this.overlayChannel}) {
@@ -23,9 +25,42 @@ class KakaoMapController with KakaoMapControllerSender {
       "cameraAnimation": animation?.toMessageable()
     });
   }
-
-  // Overlay
-  void _initalizeOverlayController() {
-
+  
+  @override
+  Future<String> addPoiStyle(List<PoiStyle> styles, [String? id]) async {
+    String styleId = await defaultLabelLayer._invokeMethod("addPoiStyle", {
+      "styleId": id,
+      "styles": styles.map((e) => e.toMessageable()).toList()
+    });
+    _poiStyle[styleId] = styles;
+    return styleId;
   }
+
+  @override
+  Future<LabelController> addLabelLayer(String id,
+      {CompetitionType competitionType = LabelController.defaultCompetitionType,
+      CompetitionUnit competitionUnit = LabelController.defaultCompetitionUnit,
+      OrderingType orderingType = LabelController.defaultOrderingType,
+      int zOrder = LabelController.defaultZOrder}) async {
+    final labelLayer = LabelController._(
+      overlayChannel,
+      this,
+      id,
+      competitionType: competitionType,
+      competitionUnit: competitionUnit,
+      orderingType: orderingType,
+      zOrder: zOrder,
+    );
+    await labelLayer._createLabelLayer();
+    _labelController[id] = labelLayer;
+    return labelLayer;
+  }
+
+  @override
+  LabelController? getLabelLayer(String id) => _labelController[id];
+
+  @override
+  LabelController get defaultLabelLayer => _labelController[OverlayManager._defaultKey]!;
+
+  /* Sender(Label) */
 }
