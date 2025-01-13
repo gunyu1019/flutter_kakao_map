96b1801f2ca072b6964191b239ec4dc615aef68c
gunyu1019 <gunyu1019@gmail.com>
2025-01-13 11:27:10 +0000

    [Feat] #4 - Implement removePoi, changePoiOffsetPosition, changePoiVisible, changePoiStyle, changePoiText method.

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/controller/overlay/OverlayController.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/controller/overlay/OverlayController.kt
@@ -6,8 +6,10 @@ import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelManager
 import com.kakao.vectormap.label.LabelLayerOptions
 import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.LatLng
 import kr.yhs.flutter_kakao_map.controller.overlay.handler.LabelControllerHandler
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asMap
+import kr.yhs.flutter_kakao_map.converter.LabelTypeConverter.asLabelTextBuilder
 import kr.yhs.flutter_kakao_map.model.OverlayType
 import android.util.Log
 
@@ -39,7 +41,76 @@ class OverlayController(
             throw NullPointerException("LabelManager is null.");
         }
         val layer = labelManager!!.getLayer(layerId)
-        val label = layer!!.addLabel(poi)
+        val label = layer.addLabel(poi)
         onSuccess.invoke(label.getLabelId())
     }
+
+    override fun removePoi(layerId: String, poiId: String, onSuccess: Function1<Any?, Unit>) {
+        if (labelManager == null) {
+            throw NullPointerException("LabelManager is null.");
+        }
+        val layer = labelManager!!.getLayer(layerId)
+        val poi = layer.getLabel(poiId)
+        layer.remove(poi)
+        onSuccess.invoke(null)
+    }
+
+    override fun changePoiOffsetPosition(layerId: String, poiId: String, x: Float, y: Float, forceDpScale: Boolean?, onSuccess: Function1<Any?, Unit>) {
+        if (labelManager == null) {
+            throw NullPointerException("LabelManager is null.");
+        }
+        val layer = labelManager!!.getLayer(layerId)
+        val poi = layer.getLabel(poiId)
+        if (forceDpScale == null) {
+            poi.changePixelOffset(x, y)
+        } else {
+            poi.changePixelOffset(x, y, forceDpScale)
+        }
+        onSuccess.invoke(null)
+    }
+
+    override fun changePoiVisible(layerId: String, poiId: String, visible: Boolean, onSuccess: Function1<Any?, Unit>) { 
+        if (labelManager == null) {
+            throw NullPointerException("LabelManager is null.");
+        }
+        val layer = labelManager!!.getLayer(layerId)
+        val poi = layer.getLabel(poiId)
+        if (visible) {
+            poi.show()
+        } else {
+            poi.hide()
+        }
+        onSuccess.invoke(null)
+    }
+
+    override fun changePoiStyle(layerId: String, poiId: String, styleId: String, onSuccess: Function1<Any?, Unit>) { 
+        if (labelManager == null) {
+            throw NullPointerException("LabelManager is null.");
+        }
+        val layer = labelManager!!.getLayer(layerId)
+        val poiStyle = labelManager!!.getLabelStyles(styleId)
+        val poi = layer.getLabel(poiId)
+        poi.changeStyles(poiStyle)
+        onSuccess.invoke(null)
+     }
+
+    override fun changePoiText(layerId: String, poiId: String, text: String, onSuccess: Function1<Any?, Unit>) {
+        if (labelManager == null) {
+            throw NullPointerException("LabelManager is null.");
+        }
+        val layer = labelManager!!.getLayer(layerId)
+        val poi = layer.getLabel(poiId)
+        poi.changeText(text.asLabelTextBuilder())
+        onSuccess.invoke(null)
+    }
+
+    override fun invalidatePoi(layerId: String, poiId: String, styleId: String, text: String, transition: Boolean, onSuccess: Function1<Any?, Unit>) { }
+
+    override fun invalidatePoi(layerId: String, poiId: String, position: LatLng, millis: Double?, onSuccess: Function1<Any?, Unit>) { }
+
+    override fun rotatePoi(layerId: String, poiId: String, angle: Double, millis: Double?, onSuccess: Function1<Any?, Unit>) { }
+
+    override fun scalePoi(layerId: String, poiId: String, x: Double, y: Double, millis: Double?, onSuccess: Function1<Any?, Unit>) { }
+
+    override fun rankPoi(layerId: String, poiId: String, rank: Long, onSuccess: Function1<Any?, Unit>) { }
 }
