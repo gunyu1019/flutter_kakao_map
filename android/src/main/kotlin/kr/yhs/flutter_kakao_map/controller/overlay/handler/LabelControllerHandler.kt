7da2f2fbd133e5944ebbc3b00ae6987d3b7baca8
gunyu1019 <gunyu1019@gmail.com>
2025-01-14 11:13:32 +0000

    [Feat] #4 - Change logic of labelHandle.

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/controller/overlay/handler/LabelControllerHandler.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/controller/overlay/handler/LabelControllerHandler.kt
@@ -2,6 +2,8 @@ package kr.yhs.flutter_kakao_map.controller.overlay.handler
 
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodCall
+import com.kakao.vectormap.label.Label
+import com.kakao.vectormap.label.LabelLayer
 import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelManager
 import com.kakao.vectormap.label.LabelLayerOptions
@@ -14,42 +16,56 @@ import kr.yhs.flutter_kakao_map.converter.LabelTypeConverter.asLabelLayerOptions
 interface LabelControllerHandler {
     val labelManager: LabelManager?
 
-    fun labelHandle(call: MethodCall, result: MethodChannel.Result) = when (call.method) {
-        "addPoi" -> { 
-            val arguments = call.arguments!!.asMap<Any?>()
-            val layerId = call.argument<String>("layerId") ?: ""
-            val poi = arguments["poi"]!!.asLabelOptions(labelManager!!)
-            addPoi(layerId, poi, result::success)
+    fun labelHandle(call: MethodCall, result: MethodChannel.Result) {
+        val arguments = call.arguments!!.asMap<Any?>()
+        if (labelManager == null) {
+            throw NullPointerException("LabelManager is null.");
         }
-        "createLabelLayer" -> {
-            val arguments = call.arguments!!.asMap<Any?>()
-            createLabelLayer(arguments.asLabelLayerOptions(), result::success)
-        }
-        else -> result.notImplemented()
+
+        val layer = arguments["layerId"]?.let(labelManager::getLayer)
+
+        // TODO (implement label required method)
+        // val poi = arguments["poiId"]?.let(layer::getLabel)
+
+        // TODO (implement poi required method)
+        /* when (call.method) {
+            "addPoi" -> { 
+                val poiOption = arguments["poi"]!!.asLabelOptions(labelManager!!)
+                addPoi(layer, poiOption, result::success)
+            }
+            "removePoi" -> {
+                val poiId = call.argument<String>("poiId")!!
+                removePoi(layer, poi, result::success)
+            }
+            "createLabelLayer" -> {
+                createLabelLayer(arguments.asLabelLayerOptions(), result::success)
+            }
+            else -> result.notImplemented()
+        } */
     }
 
     fun createLabelLayer(options: LabelLayerOptions, onSuccess: (Any?) -> Unit);
 
-    fun addPoi(layerId: String, poi: LabelOptions, onSuccess: (String) -> Unit);
+    fun addPoi(layer: LabelLayer, poi: LabelOptions, onSuccess: (String) -> Unit);
     
-    fun removePoi(layerId: String, poiId: String, onSuccess: (Any?) -> Unit);
+    fun removePoi(layer: LabelLayer, poi: Label, onSuccess: (Any?) -> Unit);
 
     // Poi Controller
-    fun changePoiOffsetPosition(layerId: String, poiId: String, x: Float, y: Float, forceDpScale: Boolean?, onSuccess: (Any?) -> Unit);
+    fun changePoiOffsetPosition(poi: Label, x: Float, y: Float, forceDpScale: Boolean?, onSuccess: (Any?) -> Unit);
 
-    fun changePoiVisible(layerId: String, poiId: String, visible: Boolean, onSuccess: (Any?) -> Unit);
+    fun changePoiVisible(poi: Label, visible: Boolean, onSuccess: (Any?) -> Unit);
 
-    fun changePoiStyle(layerId: String, poiId: String, styleId: String, onSuccess: (Any?) -> Unit);
+    fun changePoiStyle(poi: Label, styleId: String, onSuccess: (Any?) -> Unit);
 
-    fun changePoiText(layerId: String, poiId: String, text: String, onSuccess: (Any?) -> Unit);
+    fun changePoiText(poi: Label, text: String, onSuccess: (Any?) -> Unit);
 
-    fun invalidatePoi(layerId: String, poiId: String, styleId: String, text: String, transition: Boolean, onSuccess: (Any?) -> Unit);
+    fun invalidatePoi(poi: Label, styleId: String, text: String, transition: Boolean, onSuccess: (Any?) -> Unit);
 
-    fun movePoi(layerId: String, poiId: String, position: LatLng, millis: Int?, onSuccess: (Any?) -> Unit);
+    fun movePoi(poi: Label, position: LatLng, millis: Int?, onSuccess: (Any?) -> Unit);
 
-    fun rotatePoi(layerId: String, poiId: String, angle: Float, millis: Int?, onSuccess: (Any?) -> Unit);
+    fun rotatePoi(poi: Label, angle: Float, millis: Int?, onSuccess: (Any?) -> Unit);
     
-    fun scalePoi(layerId: String, poiId: String, x: Float, y: Float, millis: Int?, onSuccess: (Any?) -> Unit);
+    fun scalePoi(poi: Label, x: Float, y: Float, millis: Int?, onSuccess: (Any?) -> Unit);
 
-    fun rankPoi(layerId: String, poiId: String, rank: Long, onSuccess: (Any?) -> Unit);
+    fun rankPoi(poi: Label, rank: Long, onSuccess: (Any?) -> Unit);
 }
