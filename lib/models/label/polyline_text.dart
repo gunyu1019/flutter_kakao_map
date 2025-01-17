dd91aae55511b5025a4074be372f868d7fc9acce
gunyu1019 <gunyu1019@gmail.com>
2025-01-17 13:49:02 +0000

    [Feat] #4 - Implement some method at polyline_text

--- lib/models/label/polyline_text.dart
+++ lib/models/label/polyline_text.dart
@@ -4,18 +4,42 @@ part of '../../flutter_kakao_map.dart';
 class PolylineText {
   final LabelController _controller;
   final String id;
+  final List<LatLng> points;
 
-  PolylineText._(this._controller, this.id);
+  List<PolylineTextStyle> _styles;
+  List<PolylineTextStyle> get styles => _styles;
 
-  Future<void> changeStyles(List<PolylineTextStyle> styles) async {}
+  String _text;
+  String get text => _text;
+
+  bool _visible;
+  bool get visible => _visible;
+
+  PolylineText._(this._controller, this.id, {
+    required List<PolylineTextStyle> styles,
+    required String text,
+    required this.points,
+    bool visible = true
+  }) : _styles = styles, _text = text, _visible = visible;
+
+  Future<void> changeStyles(List<PolylineTextStyle> styles) async {
+    _styles = styles;
+  }
   
-  Future<void> changeTextAndStyles(String text, List<PolylineTextStyle> styles) async {}
+  Future<void> changeTextAndStyles(String text, List<PolylineTextStyle> styles) async {
+    _styles = styles;
+    _text = text;
+  }
 
-  Future<void> hide() async {
+  Future<void> remove() async {
 
   }
 
-  Future<void> show() async {
+  Future<void> hide() async {
+    _visible = false;
+  }
 
+  Future<void> show() async {
+    _visible = true;
   }
 }
