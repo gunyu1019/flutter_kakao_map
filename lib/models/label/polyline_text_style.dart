d77ae0ea84972ea2803edf6667b96dfc57969f37
gunyu1019 <gunyu1019@gmail.com>
2025-01-19 11:11:33 +0000

    [Feat] #4 - Add addStyle, getStyle and removeStyle method in PolylineTextStyle.

--- lib/models/label/polyline_text_style.dart
+++ lib/models/label/polyline_text_style.dart
@@ -5,14 +5,54 @@ class PolylineTextStyle {
   int zoomLevel;
 
   final int size;
-  final int strokeSize;
+  final int? strokeSize;
   final Color color;
-  final Color strokeColor;
+  final Color? strokeColor;
 
-  PolylineTextStyle(this.size, this.strokeSize, this.color, this.strokeColor, {
+  final List<PolylineTextStyle> _styles = [];
+  final bool _isSecondaryStyle;
+
+  PolylineTextStyle(this.size, this.color, {
+    this.strokeSize,
+    this.strokeColor,
     this.applyDpScale = true,
     this.zoomLevel = 0
-  });
+  }) : _isSecondaryStyle = false;
+
+  PolylineTextStyle._(this.size, this.color, {
+    this.strokeSize,
+    this.strokeColor,
+    this.applyDpScale = true,
+    this.zoomLevel = 0
+  }) : _isSecondaryStyle = true;
+
+  void addStyle(int zoomLevel, {
+    int? size,
+    Color? color,
+    bool? applyDpScale,
+    int? strokeSize,
+    Color? strokeColor
+  }) {
+    if (_isSecondaryStyle) return;
+
+    final otherStyle = PolylineTextStyle._(
+      size ?? this.size,
+      color ?? this.color,
+      applyDpScale: applyDpScale ?? this.applyDpScale,
+      strokeSize: strokeSize ?? this.strokeSize,
+      strokeColor: strokeColor ?? this.strokeColor,
+      zoomLevel: zoomLevel
+    );
+    _styles.add(otherStyle);
+  }
+
+  PolylineTextStyle? getStyle(int zoomLevel) {
+    return _styles.where((e) => e.zoomLevel == zoomLevel).firstOrNull;
+  }
+
+  void removeStyle(int zoomLevel) {
+    _styles.removeWhere((e) => e.zoomLevel == zoomLevel);
+  }
 
   Map<String, dynamic> toMessageable() {
     final payload = <String, dynamic>{
@@ -22,8 +62,11 @@ class PolylineTextStyle {
       "size": size,
       "strokeSize": strokeSize,
       // ignore: deprecated_member_use
-      "strokeColor": strokeColor.value,
+      "strokeColor": strokeColor?.value,
     };
+    if (!_isSecondaryStyle) {
+      payload['otherStyle'] = _styles.map((e) => e.toMessageable()).toList();
+    }
     return payload;
   }
 }
