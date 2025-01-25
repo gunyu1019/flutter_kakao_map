b31706358349d4f8a653da08a4e41eac01d8a9be
gunyu1019 <gunyu1019@gmail.com>
2025-01-25 15:01:58 +0000

    [Feat] #5 - (WIP) ShapeTypeConverter

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ShapeTypeConverter.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ShapeTypeConverter.kt
@@ -1,6 +1,11 @@
 package kr.yhs.flutter_kakao_map.converter
 
+import com.kakao.vectormap.shape.PolygonStyle
+import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asMap
+
 
 object ShapeTypeConverter {
-    // fun Any.asPolygonStyle(): Polygon
+    /* fun Any.asPolygonStyle(): PolygonStyle = asMap<Any?>().let { rawPayload: Map<String, Any?>
+        
+    } */
 }
