9a3eeb67c9b85616a791d902a60fdc9992ded4e5
gunyu1019 <gunyu1019@gmail.com>
2025-01-26 10:57:41 +0000

    [Feat] #5 - Add asDotPoints and asMapPoints at ShapeTypeConverter.kt

--- android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ShapeTypeConverter.kt
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_map/converter/ShapeTypeConverter.kt
@@ -6,13 +6,20 @@ import com.kakao.vectormap.shape.PolygonStyles
 import com.kakao.vectormap.shape.PolylineStyles
 import com.kakao.vectormap.shape.PolygonStylesSet
 import com.kakao.vectormap.shape.PolylineStylesSet
+import com.kakao.vectormap.shape.PolygonOptions
+import com.kakao.vectormap.shape.DotPoints
+import com.kakao.vectormap.shape.MapPoints
+import com.kakao.vectormap.shape.LatLngVertex
+import com.kakao.vectormap.shape.PointVertex
 import com.kakao.vectormap.utils.MapUtils
+import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asBoolean
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asInt
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asString
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asDouble
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asFloat
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asMap
 import kr.yhs.flutter_kakao_map.converter.PrimitiveTypeConverter.asList
+import kr.yhs.flutter_kakao_map.converter.CameraTypeConverter.asLatLng
 
 
 object ShapeTypeConverter {
@@ -67,5 +74,61 @@ object ShapeTypeConverter {
                 addPolylineStyles(element.asPolylineStyles())    
             }
          }
+    }
+
+    fun Any.asDotPoints(): DotPoints = asMap<Any?>().let { rawPayload: Map<String, Any?> ->
+        val basePoint = rawPayload["basePoint"]!!.asLatLng()
+        when(rawPayload["dotType"]!!.asInt()) {
+            0 -> DotPoints.fromCircle(
+                basePoint,
+                rawPayload["radius"]!!.asFloat(), 
+                rawPayload["clockwise"]?.asBoolean() ?: true
+            )
+            1 -> DotPoints.fromRectangle(
+                basePoint,
+                rawPayload["width"]!!.asFloat(), 
+                rawPayload["height"]!!.asFloat(), 
+                rawPayload["clockwise"]?.asBoolean() ?: true
+            )
+            else -> throw NotImplementedError()
+        }.apply<DotPoints> { 
+            rawPayload["holes"]?.asList<Any>()?.map { element ->
+                val elementPayload = element.asMap<Any?>()
+                when(rawPayload["dotType"]!!.asInt()) {
+                    0 -> PointVertex.fromCircle(
+                        elementPayload["radius"]!!.asFloat(), 
+                        elementPayload["clockwise"]?.asBoolean() ?: true
+                    )
+                    1 -> PointVertex.fromRectangle(
+                        elementPayload["width"]!!.asFloat(), 
+                        elementPayload["height"]!!.asFloat(), 
+                        elementPayload["clockwise"]?.asBoolean() ?: true
+                    )
+                    else -> throw NotImplementedError()
+                }
+            }?.let {
+                setHolePoints(*it.toTypedArray())
+            }
+         }
+    }
+
+    fun Any.asMapPoints(): MapPoints = asMap<Any?>().let { rawPayload: Map<String, Any?> ->
+        return MapPoints.fromLatLng(
+            rawPayload["points"]!!.asList<Any>().map { it.asLatLng() }
+        ).apply { 
+            rawPayload["holes"]?.asList<Any>()?.map { element ->
+                LatLngVertex.from(
+                    element.asList<Any>().map {
+                        it.asLatLng()
+                    }
+                )
+            }?.let {
+                setHolePoints(*it.toTypedArray())
+            }
+         }
+    }
+
+    fun Any.asPolygonOption(): PolygonOptions = asMap<Any?>().let { rawPayload: Map<String, Any?> ->
+        return PolygonOptions.from()
     }
 }
