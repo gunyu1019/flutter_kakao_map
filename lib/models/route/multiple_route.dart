287b5878d8874d1426c55086e94a31e41d1b5f31
gunyu1019 <gunyu1019@gmail.com>
2025-01-29 06:34:05 +0000

    [Feat] #6 - Implement multiple route.

--- lib/models/route/multiple_route.dart
+++ lib/models/route/multiple_route.dart
@@ -2,5 +2,28 @@ part of '../../flutter_kakao_maps.dart';
 
 
 class MultipleRoute {
-  
+  final RouteController _controller;
+  final String id;
+
+  final List<CurveType> _curveType;
+  final List<List<LatLng>> _points;
+  final List<int> _styleIndex;
+  final List<RouteStyle> _styles;
+
+  MultipleRoute._(this._controller, this.id,
+      {required List<List<LatLng>> points,
+      required List<RouteStyle> style,
+      required List<CurveType> curveType,
+      required List<int> styleIndex})
+      : _points = points,
+        _styles = style,
+        _curveType = curveType,
+        _styleIndex = styleIndex;
+
+  Route getRoute(int index) => Route._fromMultiple(
+    _controller, id, this, 
+    points: _points[index],
+    style: _styles[_styleIndex[index]], 
+    curveType: _curveType[index]
+  );
 }
