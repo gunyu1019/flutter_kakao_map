a29beef3a4433ccff4d79619a510eb392aac236d
gunyu1019 <gunyu1019@gmail.com>
2025-01-29 15:18:41 +0000

    [Docs] Add dart-doc at KakaoMap.

--- lib/widget/map_widget.dart
+++ lib/widget/map_widget.dart
@@ -1,23 +1,61 @@
 part of '../flutter_kakao_maps.dart';
 
+/// 카카오 맵을 제공하기 위한 위젯이 담긴 객체입니다.
 class KakaoMap extends StatefulWidget {
+  /// 카카오맵을 처음 불러올 때 설정한 값입니다.
   final KakaoMapOption? option;
 
+  /// 지도를 정상적으로 불러왔을 때, 호출되는 함수입니다.
+  /// 지도 조작에 필요한 [KakaoMapController]가 매개변수에 입력됩니다.
   final void Function(KakaoMapController controller) onMapReady;
+
+  /// 지도의 상태관리를 위한 매개변수입니다.
   final KakaoMapLifecycle? onMapLifecycle;
+
+  /// 지도에서 예상치 못한 에러가 발생했을 때, 호출되는 함수입니다.
+  /// 대표적으로 네이티브 키 인증 실패했을 때, [KakaoAuthException]가 [exception]에 입력되어 호출됩니다.
   final void Function(Exception exception)? onMapError;
+
+  /// 사용자 또는 소스코드에 의해 카메라가 이동을 시작했을 때, 호출되는 함수입니다.
+  /// 사용자가 아닌 소스코드(프로그램)에 의해 이동되었다면, [GestureType.unknown]가 [gestureType]으로 입력됩니다.
   final void Function(GestureType gestureType)? onCameraMoveStart;
+  
+  /// 사용자 또는 소스코드에 의해 카메라가 이동을 마쳤을 때, 호출되는 함수입니다.
+  /// [position] 매개변수에는 이동을 마친 최종 카메라의 위치가 입력됩니다.
+  /// 사용자가 아닌 소스코드(프로그램)에 의해 이동되었다면, [GestureType.unknown]가 [gestureType]으로 입력됩니다.
   final void Function(CameraPosition position, GestureType gestureType)?
       onCameraMoveEnd;
 
+  /// 사용자에 의해 나침판이 클릭되었을 때, 호출되는 함수입니다.
   final void Function()? onCompassClick;
+
+  /// 사용자에 의해 [Poi]가 클릭되었을 때, 호출되는 함수입니다.
+  /// [labelController] 매개변수에는 [Poi]가 소속되어 있는 컨트롤러가 담긴 객체를 입력합니다.
+  /// 사용자가 선택한 [Poi]는 [poi] 매게변수에 입력됩니다.
   final void Function(LabelController labelController, Poi poi)? onPoiClick;
+  
+  /// 사용자에 의해 [LodPoi]가 클릭되었을 때, 호출되는 함수입니다.
+  /// [lodLabelController] 매개변수에는 [LodPoi]가 소속되어 있는 컨트롤러가 담긴 객체를 입력합니다.
+  /// 사용자가 선택한 [LodPoi]는 [poi] 매게변수에 입력됩니다.
   final void Function(LodLabelController lodLabelController, LodPoi poi)?
       onLodPoiClick;
+  
+  /// 사용자가 지도판을 클릭했을 때, 호출되는 함수입니다.
+  /// [point] 매개변수는 지도를 기준으로 사용자가 클릭한 좌표가 입력됩니다.
+  /// [position] 매개변수는 사용자가 클릭한 위/경도 값이 입력됩니다.
   final void Function(KPoint point, LatLng position)? onMapClick;
+  
+  /// 사용자가 [Poi], [LodPoi]를 제외한 지도의 영역을 클릭했을 때, 호출되는 함수입니다.
+  /// [point] 매개변수는 지도를 기준으로 사용자가 클릭한 좌표가 입력됩니다.
+  /// [position] 매개변수는 사용자가 클릭한 위/경도 값이 입력됩니다.
   final void Function(KPoint point, LatLng position)? onTerrainClick;
+  
+  /// 사용자가 [Poi], [LodPoi]를 제외한 지도의 영역을 길게 클릭했을 때, 호출되는 함수입니다.
+  /// [point] 매개변수는 지도를 기준으로 사용자가 클릭한 좌표가 입력됩니다.
+  /// [position] 매개변수는 사용자가 클릭한 위/경도 값이 입력됩니다.
   final void Function(KPoint point, LatLng position)? onTerrainLongClick;
 
+  /// 사용자의 제스쳐가 지도에 우선 전달될 수 있도록 설정하는 값입니다.
   final bool forceGesture;
 
   const KakaoMap(
