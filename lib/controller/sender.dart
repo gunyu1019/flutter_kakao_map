eab8d2f2f8841e45ff049193fda7f48e18f8647f
gunyu1019 <gunyu1019@gmail.com>
2025-01-29 15:23:48 +0000

    [Docs] Add dart-doc at KakaoMapControllerSender.

--- lib/controller/sender.dart
+++ lib/controller/sender.dart
@@ -9,10 +9,14 @@ abstract class KakaoMapControllerSender {
   /// [animation] 매개변수를 입력하면 이동 애니메이션을 가지게된다.
   Future<void> moveCamera(CameraUpdate camera, {CameraAnimation? animation});
 
+  /// 주어진 [x]와 [y] 값이 지도의 위/경도로 어디에 속해있는지 반환합니다.
   Future<LatLng> fromScreenPoint(double x, double y);
 
+  /// 주어진 [position] 매개변수를 통해 스크린의 x 좌표와 y 좌표 값을 [KPoint]에 담아 반환합니다.
   Future<KPoint> toScreenPoint(LatLng position);
 
+  /// 지도에서 사용할 수 있는 제스처를 설정합니다.
+  /// [gesture]는 사용 유무를 설정한 제스쳐입니다.
   Future<void> setGesture(GestureType gesture, bool enable);
 
   // Future<void> clearCache();
