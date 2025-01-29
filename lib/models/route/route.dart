726303084092468346a92fa1a7880474ee18c7c4
gunyu1019 <gunyu1019@gmail.com>
2025-01-29 06:43:12 +0000

    [Feat] #6 - Implement route management feature.

--- lib/models/route/route.dart
+++ lib/models/route/route.dart
@@ -13,11 +13,50 @@ class Route {
   CurveType _curveType;
   CurveType get curveType => _curveType;
 
+  final bool _isMultiple;
+  final MultipleRoute? parents;
+
+  bool _visible;
+  bool get visible => _visible;
+
   Route._(this._controller, this.id,
       {required List<LatLng> points,
       required RouteStyle style,
       required CurveType curveType})
       : _points = points,
         _style = style,
-        _curveType = curveType;
+        _curveType = curveType,
+        _isMultiple = false,
+        _visible = true,
+        parents = null;
+
+  Route._fromMultiple(this._controller, this.id, this.parents,
+      {required List<LatLng> points,
+      required RouteStyle style,
+      required CurveType curveType})
+      : _points = points,
+        _style = style,
+        _curveType = curveType,
+        _visible = true,
+        _isMultiple = true;
+
+  Future<void> show() async {
+    _visible = true;
+  }
+
+  Future<void> hide() async {
+    _visible = false;
+  }
+
+  Future<void> remove() async {
+    await _controller.removeRoute(this);
+  }
+
+  Future<void> changeStyle() async {
+    
+  }
+
+  Future<void> changePoint() async {
+    
+  }
 }
