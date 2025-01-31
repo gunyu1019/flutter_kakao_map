82fa2709398c36d709699bc35eb062e9cac2daa9
gunyu1019 <gunyu1019@gmail.com>
2025-01-31 14:41:14 +0000

    [Docs] Dart-doc camera_position.dart.

--- lib/models/camera/camera_position.dart
+++ lib/models/camera/camera_position.dart
@@ -3,10 +3,19 @@ part of '../../flutter_kakao_maps.dart';
 
 /// 지도의 카메라 속성을 가지고 있는 객체입니다.
 class CameraPosition with KMessageable {
+  /// 지도를 비추고 있는 카메라의 중심 위치를 가져옵니다.
   final LatLng position;
+
+  /// 지도의 비추고 있는 카메라의 확대/축소 값을 가져옵니다.
   final int zoomLevel;
+
+  /// 지도를 비추고 있는 카메라의 기울기 각도를 가져옵니다.
   final double? tiltAngle = null;
+
+  /// 지도를 비추고 있는 카메라의 회전 각도를 가져옵니다.
   final double? rotationAngle = null;
+
+  /// 지도를 비추고 있는 카메라의 높이를 가져옵니다.
   final double? height = null;
 
   const CameraPosition(
