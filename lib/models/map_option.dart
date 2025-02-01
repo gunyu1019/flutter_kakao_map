c50caadd097b52855d433699b1efc1bbf9787270
gunyu1019 <gunyu1019@gmail.com>
2025-02-01 04:32:46 +0000

    [Docs] Dart-doc KakaoMapOption.

--- lib/models/map_option.dart
+++ lib/models/map_option.dart
@@ -1,10 +1,20 @@
 part of '../flutter_kakao_maps.dart';
 
+/// 지도를 시작될 때, 지도의 기본 정보를 정의하는 객체입니다.
 class KakaoMapOption {
+  /// 지도가 시작하면 카메라가 비출 위치를 설정합니다.
+  /// 기본 값은 카카오 판교캠퍼스입니다.
   final LatLng position;
+
+  /// 지도가 시작하면 카메라의 확대/축소 값입니다.
   final int zoomLevel;
+
+  /// 지도의 유형입니다.
   final MapType mapType;
+
+  /// 지도 객체의 ViewName을 정의합니다.
   final String? viewName;
+
   final bool visible;
   final String? tag;
 
