5deac4414e37bd994affe18158bc07e316340132
gunyu1019 <gunyu1019@gmail.com>
2025-01-31 20:17:53 -0800

    [Feat] Apply authentication feature at iOS.

--- ios/Classes/FlutterKakaoMapsPlugin.swift
+++ ios/Classes/FlutterKakaoMapsPlugin.swift
@@ -5,11 +5,16 @@ import UIKit
 
 public class FlutterKakaoMapsPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
-        SDKInitializer.InitSDK(appKey: "SDK KEY")
-        
         let kakaoMapFactory = KakaoMapViewFactory(messagenger: registrar.messenger())
         registrar.register(kakaoMapFactory, withId: MAP_VIEW_TYPE_ID)
+        
+        let sdkChannel = FlutterMethodChannel(name: SDK_CHANNEL_NAME, binaryMessenger: registrar.messenger())
+        let _ = SdkInitializer(channel: sdkChannel)
     }
     
     private static let MAP_VIEW_TYPE_ID = "plugin/kakao_map"
+    
+    private static let SDK_CHANNEL_NAME = "flutter_kakao_maps_sdk"
+    private static let OVERLAY_CHANNEL_NAME = "flutter_kakao_maps_overlay"
+    private static let VIEW_CHANNEL_NAME = "flutter_kakao_maps_view"
 }
