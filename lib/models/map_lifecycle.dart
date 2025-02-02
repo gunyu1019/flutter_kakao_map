3061692e63d45fe36c413adc42e92e75163f41a6
gunyu1019 <gunyu1019@gmail.com>
2025-02-02 07:34:25 +0000

    [Docs] Dart-doc KakaoMapLifecycle.

--- lib/models/map_lifecycle.dart
+++ lib/models/map_lifecycle.dart
@@ -1,7 +1,13 @@
 part of '../flutter_kakao_maps.dart';
 
+/// 지도의 상태관리를 관리하는 Mixin 객체입니다.
 mixin KakaoMapLifecycle {
+  /// 지도가 삭제되면 호출되는 인수입니다.
   void Function()? onMapDestroy;
+
+  /// 지도가 일시정지되면 호출되는 인수입니다.
   void Function()? onMapPaused;
+
+  /// 지도가 일시정지에서 풀리면 호출되는 함수입니다.
   void Function()? onMapResumed;
 }
