3ca7f01a18d4bc40987ef1cf1d28bed97aef0656
gunyu1019 <gunyu1019@gmail.com>
2025-02-08 02:50:37 -0800

    [Fix] Make convenience init from init method / Wrong logic of toMessageable method.

--- ios/Classes/converter/CameraTypeConverter.swift
+++ ios/Classes/converter/CameraTypeConverter.swift
@@ -1,35 +1,51 @@
+import KakaoMapsSDK
 
 internal extension MapPoint {
-    init(payload: Dictionary<String, Any>) {
-        let convertedPayload = asDictTyped(payload, asDouble)
-        init(convertedPayload)
+    convenience init(payload: Dictionary<String, Any>) {
+        self.init(
+            longitude: asDouble(payload["longitude"]!),
+            latitude: asDouble(payload["latitude"]!)
+        )
     }
 
-    init(payload: Dictionary<String, Double>) {
-        init(
-            latitude: payload["latitude"]!,
-            longitude: payload["longitude"]!
+    convenience init(payload: Dictionary<String, Double>) {
+        self.init(
+            longitude: payload["longitude"]!,
+            latitude: payload["latitude"]!
         )
     }
 
     func toMessageable() -> Dictionary<String, Double> {
         [
-            "latitude": self.latitude,
-            "longitude": self.longitude
+            "latitude": self.wgsCoord.latitude,
+            "longitude": self.wgsCoord.longitude
         ]
     }
 }
 
-internal extension CameraUpdate {
-    init(payload: Dictionary<String, Any>) {
-        
+internal func asCameraUpdate(kakaoMap: KakaoMap, payload: Dictionary<String, Any>) -> CameraUpdate {
+    let cameraUpdateType = asInt(payload["type"]!)
+    let zoomLevel = castSafty(payload["zoomLevel"], caster: asInt)
+    let angle = castSafty(payload["angle"], caster: asDouble)
+    switch cameraUpdateType {
+    case 0: return CameraUpdate.make(target: MapPoint(payload: payload), zoomLevel: zoomLevel!, mapView: kakaoMap)
+    // case 1: return CameraUpdate.make(mapView: kakaoMap)
+    case 3: return CameraUpdate.make(zoomLevel: zoomLevel!, mapView: kakaoMap)
+    case 4: return CameraUpdate.make(zoomLevel: kakaoMap.zoomLevel+1, mapView: kakaoMap)
+    case 5: return CameraUpdate.make(zoomLevel: kakaoMap.zoomLevel-1, mapView: kakaoMap)
+    case 6: return CameraUpdate.make(rotation: angle!, tilt: kakaoMap.tiltAngle, mapView: kakaoMap)
+    case 7: return CameraUpdate.make(rotation: kakaoMap.rotationAngle, tilt: angle!, mapView: kakaoMap)
+    // case 8: return CameraUpdate.make(zoomLevel: zoomLevel!, mapView: kakaoMap)
+    default: return CameraUpdate.make(mapView: kakaoMap)
     }
 }
 
 internal extension CameraAnimationOptions {
     init(payload: Dictionary<String, Any>) {
-        self.autoElevation = asBoolean(payload["autoElevation"] ?? false )
-        self.consecutive = asBoolean(payload["autoElevation"] ?? false)
-        self.durationInMillis = asInt(payload["duration"]!)
+        self.init(
+            autoElevation: ObjCBool(asBool(payload["autoElevation"] ?? false )),
+            consecutive: ObjCBool(asBool(payload["autoElevation"] ?? false)),
+            durationInMillis: UInt(asInt(payload["duration"]!))
+        )
     }
 }
