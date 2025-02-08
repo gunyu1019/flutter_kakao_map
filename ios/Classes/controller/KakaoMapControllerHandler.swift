ca21a6212d3c5d120803a3bdf6848a5eab3d28d7
gunyu1019 <gunyu1019@gmail.com>
2025-02-08 02:52:24 -0800

    [Feat] Implement and Test KakaoMapControllerHandler

--- ios/Classes/controller/KakaoMapControllerHandler.swift
+++ ios/Classes/controller/KakaoMapControllerHandler.swift
@@ -1,10 +1,13 @@
+import KakaoMapsSDK
+import Flutter
+
 internal protocol KakaoMapControllerHandler {
     func getCameraPosition(onSuccess: @escaping (_ cameraPosition: Dictionary<String, Any>) -> Void)
 
     func moveCamera(
         cameraUpdate: CameraUpdate,
         cameraAnimation: CameraAnimationOptions?,
-        onSuccess: () -> Void
+        onSuccess: (Any?) -> Void
     )
 }
 
@@ -12,10 +15,11 @@ internal extension KakaoMapControllerHandler {
     func handle(call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
             case "getCameraPosition": getCameraPosition(onSuccess: result)
-            case "moveCamera": moveCamera(
-                
+            /* case "moveCamera": moveCamera(
+                cameraUpdate: asCameraUpdate(kakaoMap: KakaoMap, payload: asDict(call.arguments["cameraUpdate"]!)),
+                cameraAnimation: call.arguments["cameraAnimation"] == nil ? nil : CameraAnimationOptions(payload: asDict(call.arguments["cameraAnimation"]!)),
                 onSuccess: result
-            )
+            ) */
             default: result(FlutterMethodNotImplemented)
         }
     }
