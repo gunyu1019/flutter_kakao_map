585f310d8efeee9ffa6215a3759548e2882c9ecd
gunyu1019 <gunyu1019@gmail.com>
2025-02-09 19:14:41 +0800

    [Feat] #7 - Provide kakaoMap cameraPosition value instead of -1 or nil.

--- ios/Classes/converter/CameraTypeConverter.swift
+++ ios/Classes/converter/CameraTypeConverter.swift
@@ -25,26 +25,28 @@ internal extension MapPoint {
 
 internal func asCameraUpdate(kakaoMap: KakaoMap, payload: Dictionary<String, Any>) -> CameraUpdate {
     let cameraUpdateType = asInt(payload["type"]!)
-    let zoomLevel = castSafty(payload["zoomLevel"], caster: asInt)
+    let zoomLevel = castSafty(payload["zoomLevel"], caster: asInt) ?? kakaoMap.zoomLevel
     let angle = castSafty(payload["angle"], caster: asDouble)
     switch cameraUpdateType {
-    case 0: return CameraUpdate.make(target: MapPoint(payload: payload), zoomLevel: zoomLevel!, mapView: kakaoMap)
+    case 0: return CameraUpdate.make(target: MapPoint(payload: payload), zoomLevel: zoomLevel, mapView: kakaoMap)
     case 1: 
         return CameraUpdate.make(
             target: MapPoint(payload: payload),
-            zoomLevel: zoomLevel!,
-            rotation: asDouble(payload["rotationAngle"] ?? -1.0),
-            tilt: asDouble(payload["tiltAngle"] ?? -1.0),
+            zoomLevel: zoomLevel,
+            rotation: asDouble(payload["rotationAngle"] ?? kakaoMap.rotationAngle),
+            tilt: asDouble(payload["tiltAngle"] ?? kakaoMap.tiltAngle),
             mapView: kakaoMap
         )
-    case 3: return CameraUpdate.make(zoomLevel: zoomLevel!, mapView: kakaoMap)
+    case 3: return CameraUpdate.make(zoomLevel: zoomLevel, mapView: kakaoMap)
     case 4: return CameraUpdate.make(zoomLevel: kakaoMap.zoomLevel+1, mapView: kakaoMap)
     case 5: return CameraUpdate.make(zoomLevel: kakaoMap.zoomLevel-1, mapView: kakaoMap)
     case 6: return CameraUpdate.make(rotation: angle!, tilt: kakaoMap.tiltAngle, mapView: kakaoMap)
     case 7: return CameraUpdate.make(rotation: kakaoMap.rotationAngle, tilt: angle!, mapView: kakaoMap)
     case 8: return CameraUpdate.make(
-        area: AreaRect(asArray<MapPoint>(payload["points"], caster: MapPoint)),
-        levelLimit: zoomLevel ?? -1
+        area: AreaRect(points: asArray<Dictionary>(payload["points"]!, caster: asDict).map {
+            element in MapPoint(payload: element)
+        }),
+        levelLimit: zoomLevel
     )
     default: return CameraUpdate.make(mapView: kakaoMap)
     }
