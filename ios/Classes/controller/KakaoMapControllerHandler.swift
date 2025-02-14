b63f132de7d7e2a07a9d685b6841872c825c381f
gunyu1019 <gunyu1019@gmail.com>
2025-02-13 23:53:47 -0800

    [Feat] Implement setGestureEnable method at KakaoMapControllerHandler.swift

--- ios/Classes/controller/KakaoMapControllerHandler.swift
+++ ios/Classes/controller/KakaoMapControllerHandler.swift
@@ -14,22 +14,28 @@ internal protocol KakaoMapControllerHandler {
 
     func setEventHandler(event: UInt8)
 
-    func setGestureEnable(gestureType: GestrueType, enable: Bool, onSuccess: (Any?) -> Void)
+    func setGestureEnable(gestureType: GestureType, enable: Bool, onSuccess: (Any?) -> Void)
 }
 
 internal extension KakaoMapControllerHandler {
     func handle(call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
-            case "getCameraPosition": getCameraPosition(onSuccess: result)
-            case "moveCamera":
+        case "getCameraPosition": getCameraPosition(onSuccess: result)
+        case "moveCamera":
             let arguments = asDict(call.arguments!)
             let cameraUpdate = asCameraUpdate(kakaoMap: self.kakaoMap, payload: asDict(arguments["cameraUpdate"]!))
             let rawCameraAnimation = castSafty(arguments["cameraAnimation"], caster: asDict)
             let cameraAnimation = rawCameraAnimation != nil ? CameraAnimationOptions(payload: rawCameraAnimation!) : nil
             moveCamera(cameraUpdate: cameraUpdate, cameraAnimation: cameraAnimation, onSuccess: result)
-            case "setEventHandler": setEventHandler(event: (call.arguments! as! UInt8))
-            // case "setGestureEnable":
-            default: result(FlutterMethodNotImplemented)
+        case "setEventHandler": setEventHandler(event: (call.arguments! as! UInt8))
+        case "setGestureEnable":
+            let arguments = asDict(call.arguments!)
+            setGestureEnable(
+                gestureType: GestureType(rawValue: asInt(arguments["gestureType"]!))!,
+                enable: asBool(arguments["enable"]!),
+                onSuccess: result
+            )
+        default: result(FlutterMethodNotImplemented)
         }
     }
 }
