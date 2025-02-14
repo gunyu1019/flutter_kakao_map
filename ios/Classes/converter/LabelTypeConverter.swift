9029512c8b78ff0f1522067be6b803e0e2ff6a2b
gunyu1019 <gunyu1019@gmail.com>
2025-02-14 11:29:53 +0000

    [Feat] Implement Label Type Converter (WIP)

--- ios/Classes/converter/LabelTypeConverter.swift
+++ ios/Classes/converter/LabelTypeConverter.swift
@@ -1,17 +1,31 @@
 import KakaoMapsSDK
 
 
-internal extension TextStyle {
+internal func asPoiTransition(payload: Dictionary<String, Any>) -> PoiTransition {
+    return PoiTransition(
+        enterence: TransitionType(rawValue: asString(payload["enterence"]) ?? .none,
+        exit: TransitionType(rawValue: asString(payload["exit"])) ?? .none
+    )
+}
+
+internal extension PoiIconStyle {
+    convenience init(paylaod: Dictionary<String, Any>) {
+        let transition = asPoiTransition(castSafty(payload["iconTransition"], caster: asDict))
+        self.init(
+            symbol: UIImage?,
+            anchorPoint: castSafty(payload["anchor"], caster: CGPoint(payload: $0, caster: asDouble)) ?? CGPoint(x: 0.5, y: 0.5),
+            transition: transition,
+            enableEntranceTransition: transition.enterence != .none,
+            enableExitTransition: transition.exit = .none,
+            // badges: [PoiBadge]? = nil (TODO)
+        )
+    }
+}
+
+internal extension PerLevelPoiStyle {
     convenience init(payload: Dictionary<String, Any>) {
         self.init(
-            fontSize: asInt(rawPayload["size"]!),
-            fontColor: UIColor(asInt(rawPayload["color"]!!)),
-            strokeThickness: castSafty(payload["strokeSize"], caster: asInt) ?? 0,
-            strokeColor: castSafty(payload["strokeColor"], caster: UIColor(asInt($0))) ?? UIColor.white,
-            font: castSafty(payload["font"], caster: asString) ?? "",
-            charSpace: castSafty(payload["characterSpace"], caster: asInt) ?? 0,
-            lineSpace: castSafty(payload["lineSpace"], caster: asDouble) ?? 1.0,
-            aspectRatio: castSafty(payload["aspectRatio"], caster: asDouble) ?? 1.0
+
         )
     }
 }
@@ -21,9 +35,9 @@ internal extension LabelLayerOptions {
     convenience init(payload: Dictionary<String, Any>) {
         self.init(
             layerID: asString(payload["layerId"]!),
-            competitionType: castSafty(payload["competitionType"], caster: CompetitionType(rawValue: asInt($0))) ?? CompetitionType.CompetitionTypeNone,
-            competitionUnit: castSafty(payload["competitionUnit"], caster: CompetitionUnit(rawValue: asInt($0))) ?? CompetitionType.CompetitionUnitPoi,
-            orderType: castSafty(payload["orderType"], caster: OrderingType(rawValue: asInt($0))) ?? OrderingType.OrderingTypeRank,
+            competitionType: castSafty(payload["competitionType"], caster: CompetitionType(rawValue: asInt($0))) ?? CompetitionType.none,
+            competitionUnit: castSafty(payload["competitionUnit"], caster: CompetitionUnit(rawValue: asInt($0))) ?? CompetitionType.poi,
+            orderType: castSafty(payload["orderType"], caster: OrderingType(rawValue: asInt($0))) ?? OrderingType.rank,
             zOrder: castSafty(payload["zOrder"], caster: asInt) ?? 10001
         )
     }
