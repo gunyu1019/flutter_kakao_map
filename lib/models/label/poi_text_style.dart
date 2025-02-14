a15d315f9c4f062b3f800832fd3ded35ba0cbb32
gunyu1019 <gunyu1019@gmail.com>
2025-02-14 11:29:05 +0000

    [Docs] Add docstring at PoiTextStyle.

--- lib/models/label/poi_text_style.dart
+++ lib/models/label/poi_text_style.dart
@@ -1,13 +1,31 @@
 part of '../../kakao_map_sdk.dart';
 
+
+/// Poi에서 사용되는 텍스트의 스타일을 정의하는 객체입니다.
+/// [Poi]와 [LodPoi]에서 사용되는 텍스트의 스타일을 정의할 때 사용됩니다.
 class PoiTextStyle with KMessageable {
+  /// 글씨 장평
   final double aspectRatio;
+
+  /// 글씨 자간
   final int characterSpace;
+
+  /// 글씨 색상
   final Color color;
+
+  /// 글씨 폰트
   final String font;
+   
+  /// 글씨 행간
   final double lineSpace;
+
+  /// 글씨 크기
   final int size;
+
+  /// 글씨 외곽선의 두께
   final int stroke;
+
+  /// 글씨 외곽선의 색상
   final Color strokeColor;
 
   const PoiTextStyle(
