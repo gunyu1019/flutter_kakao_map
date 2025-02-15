622d219a2c93071a1b65e7f2cf7a9ed3f60c4784
gunyu1019 <gunyu1019@gmail.com>
2025-02-14 23:32:12 -0800

    [Fix] Missing swift grammer at ReferenceTypeConverter.swift

--- ios/Classes/converter/ReferenceTypeConverter.swift
+++ ios/Classes/converter/ReferenceTypeConverter.swift
@@ -1,4 +1,5 @@
 import KakaoMapsSDK
+import Flutter
 
 
 internal extension UIColor {
@@ -28,37 +29,32 @@ internal extension UIImage {
 
 
 internal func asImage(payload: Dictionary<String, Any>) -> UIImage? {
-    let width = asInt(payload["width"])
-    let height = asInt(payload["height"])
+    let width = asInt(payload["width"]!)
+    let height = asInt(payload["height"]!)
     let size = CGSize(width: width, height: height)
-    let imageType = asInt(payload["type"])
+    let imageType = asInt(payload["type"]!)
     switch imageType {
     case 0:
-        let rawPath = asString(payload["path"])
+        let rawPath = asString(payload["path"]!)
         let path = FlutterKakaoMapsPlugin.getAssets(path: rawPath)
-        return UIImage(contentsOfFile: path).targetSize(size: size)
+        return UIImage(contentsOfFile: path)?.resize(size: size)
     case 2:
         let data = payload["data"] as! FlutterStandardTypedData
-        return UIImage(data: data.data).targetSize(size: size)
+        return UIImage(data: data.data)?.resize(size: size)
     default:  // type 1
-        let path = asString(payload["path"])
-        return UIImage(contentsOfFile: path).targetSize(size: size)
+        let path = asString(payload["path"]!)
+        return UIImage(contentsOfFile: path)?.resize(size: size)
     }
 }
 
 
 internal extension CGPoint {
-    convenience init(payload: Dictionary<String, Double>) {
-        self.init(x: payload["x"]!, y: paylaod["y"]!)
+    init(payload: Dictionary<String, Double>) {
+        self.init(x: payload["x"]!, y: payload["y"]!)
     }
 
-    convenience init(payload: Dictionary<String, Int>) {
-        self.init(x: payload["x"]!, y: paylaod["y"]!)
-    }
-
-    convenience init<T>(payload: Dictionary<String, Any>, caster: (Any) throws -> T) {
-        let rawPayload = asDictTyped(payload, caster: caster)
-        self.init(x: rawPayload["x"]!, y: rawPayload["y"]!)
+    init(payload: Dictionary<String, Int>) {
+        self.init(x: payload["x"]!, y: payload["y"]!)
     }
 }
 
@@ -66,14 +62,14 @@ internal extension CGPoint {
 internal extension TextStyle {
     convenience init(payload: Dictionary<String, Any>) {
         self.init(
-            fontSize: asInt(rawPayload["size"]!),
-            fontColor: UIColor(asInt(rawPayload["color"]!!)),
-            strokeThickness: castSafty(payload["strokeSize"], caster: asInt) ?? 0,
-            strokeColor: castSafty(payload["strokeColor"], caster: UIColor(asInt($0))) ?? UIColor.white,
+            fontSize: payload["size"] as! UInt,
+            fontColor: UIColor(value: payload["color"] as! UInt),
+            strokeThickness: castSafty(payload["strokeSize"], caster: asUInt) ?? 0,
+            strokeColor: castSafty(payload["strokeColor"], caster: { UIColor(value: asUInt($0)) }) ?? UIColor.white,
             font: castSafty(payload["font"], caster: asString) ?? "",
             charSpace: castSafty(payload["characterSpace"], caster: asInt) ?? 0,
-            lineSpace: castSafty(payload["lineSpace"], caster: asDouble) ?? 1.0,
-            aspectRatio: castSafty(payload["aspectRatio"], caster: asDouble) ?? 1.0
+            lineSpace: castSafty(payload["lineSpace"], caster: asFloat) ?? 1.0,
+            aspectRatio: castSafty(payload["aspectRatio"], caster: asFloat) ?? 1.0
         )
     }
 }
