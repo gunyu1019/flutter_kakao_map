3cf7fbc49ccbb8f95aacf28f5d765a798d64fc89
gunyu1019 <gunyu1019@gmail.com>
2025-02-16 13:00:36 +0000

    [Docs] Dart-Doc PoiTransition

--- lib/models/label/poi_transition.dart
+++ lib/models/label/poi_transition.dart
@@ -1,7 +1,11 @@
 part of '../../kakao_map_sdk.dart';
 
+/// [Poi] 또는 [LodPoi]가 나타나고, 사라질 때 적용하는 애니메이션을 정의합니다.
 class PoiTransition with KMessageable {
+  /// [Poi] 또는 [LodPoi]가 나타날 때 적용되는 애니메이션입니다.
   final Transition entrance;
+
+  /// [Poi] 또는 [LodPoi]가 사라질 때 적용되는 애니메이션입니다.
   final Transition exit;
 
   const PoiTransition(
