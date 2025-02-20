46f91aea01abc5e131bfe66b7c539d2c785b0b75
gunyu1019 <gunyu1019@gmail.com>
2025-02-20 11:29:47 +0000

    [Docs] Add docs at PolylineTextStyle

--- lib/models/label/polyline_text_style.dart
+++ lib/models/label/polyline_text_style.dart
@@ -1,12 +1,24 @@
 part of '../../kakao_map_sdk.dart';
 
+/// [PolylineText]의 스타일을 표현하는 객체입니다.
 class PolylineTextStyle {
+  /// (Android 한정된 기능) 픽셀 밀도에 따라 텍스트 크기를 조정 유무를 정의합니다.
   bool applyDpScale;
+
+  /// [PolylineTextStyle]이 나타날 [zoomLevel]을 설정합니다.
+  /// [PolylineTextStyle.zoomLevel]값이 카메라의 [CameraPosition.zoomLevel] 값보다 작으면 해당되는 [PolylineTextStyle]이 적용됩니다.
   int zoomLevel;
 
+  /// 글씨 크기
   final int size;
+
+  /// 글씨 외곽선의 두께
   final int? strokeSize;
+
+  /// 글씨 색상
   final Color color;
+
+  /// 글씨 외곽선의 색상
   final Color? strokeColor;
 
   final List<PolylineTextStyle> _styles = [];
@@ -26,6 +38,10 @@ class PolylineTextStyle {
       this.zoomLevel = 0})
       : _isSecondaryStyle = true;
 
+  /// [zoomLevel]에 따라 [PolylineTextStyle]에 표시될 다른 스타일을 정의합니다.
+  /// 메소드에서 사용된 [zoomLevel] 매개변수가 [CameraPosition.zoomLevel] 값보다 작으면
+  /// [PolylineTextStyle.addStyle] 메소드로 정의한 새로운 스타일이 적용됩니다.
+  /// 같은 [PolylineTextStyle] 객체에서 다른 스타일을 정의할 때, [zoomLevel] 매개변수의 값이 중복되서는 안됩니다.
   void addStyle(int zoomLevel,
       {int? size,
       Color? color,
